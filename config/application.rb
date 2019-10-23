19feede817a014ae080bf29f729d11baf6af6a7d
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-10-23 21:30:41 +0200

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Todos
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: :any
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
